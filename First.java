3fea63d29abe207aabc0b32570923036d7f53415
Anuj <iamanujsanjeet@gmail.com>
2024-12-04 19:06:26 +0530

    Anything

---
+++ First.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class First {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        int []arr={1,2,3,45,5};
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println(arr[j]);
+            
+        }
+    }
+}
